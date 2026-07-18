46d704dc2f5497415eff39f5d4344d5c0ef22ff0
Rifat Bin Aziz <rifatbinaziz.1998@gmail.com>
2026-07-18 20:42:30 +0600

    feat: add serilog, sinks file

--- SmartTaskManagement.API/Program.cs
+++ SmartTaskManagement.API/Program.cs
@@ -1,10 +1,17 @@
+using Serilog;
 using SmartTaskManagement.Infrastructure;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Host.UseSerilog((context, services, configuration) => configuration
+    .ReadFrom.Configuration(context.Configuration)
+    .ReadFrom.Services(services));
+
 builder.Services.AddInfrastructure(builder.Configuration);
 
 var app = builder.Build();
+
+app.UseSerilogRequestLogging();
 
 app.MapGet("/", () => "Hello World!");
 
